4e72d2e487d66e28b352b3b1fd745b942919d40a
Iván R. Galicia <irodgal@gmail.com>
2016-02-09 16:43:07 +0100

    Primeros test con mocha

--- src/server.js
+++ src/server.js
@@ -3,39 +3,37 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var request = require('request');
+var parseString = require('xml2js').parseString;
 
 //enviamos nuestro archivo index.html al usuario como página de inicio
-app.get('/nflscores', function(req, res){
+app.get('/nflscores', function(req, res) {
 
 	var urlRegularSeason = {
 		url: 'http://www.nfl.com/liveupdate/scorestrip/ss.xml',
-		method  : 'GET'
+		method: 'GET'
 	};
 
 	var urlPostSeason = {
 		url: 'http://www.nfl.com/liveupdate/scorestrip/postseason/ss.xml',
-		method  : 'GET'
+		method: 'GET'
 	};
 
 	//var bosy2;
-	request(urlRegularSeason, function (error, response, body) {
+	request(urlRegularSeason, function(error, response, body) {
 
-	    if (!error && response.statusCode == 200) {
-	       //console.log(body);
-	       // console.log("PEPE");
-	       // console.log(res);
-	        //bosy2 = body;
-	        //res.json({"pepe":"pepe2"});
-	        //res.write(JSON.stringify(body));
-	        //res.sendFile(path.join(__dirname + '/index.html'));
-	        //res.send(JSON.stringify(body));
-	        res.setHeader('Content-Type', 'application/json');
-	        res.send(body);
-	    }
+		if (!error && response.statusCode == 200) {
+			res.setHeader('Content-Type', 'application/json');
+
+			//en body tenemos el xml desde nfl.com
+			var objToResponse;
+
+			parseString(body, function(err, result) {
+				objToResponse = result;
+			});
+
+			res.send(JSON.stringify(objToResponse));
+		}
 	});
-	//console.log(bosy2);
-	//res.json(bosy2);
-	//res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 //iniciamos el servidor
